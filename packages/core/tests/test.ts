a29e7749b8d56fb08e1f8128c1689abaadb96a49
Aron <i@aron.tw>
2024-02-28 00:38:24 +0800

    Update test case for generating color scale

--- packages/core/tests/test.ts
+++ packages/core/tests/test.ts
@@ -1,12 +1,12 @@
 import fillColorScale from '../src/fill-color-scale'
 import colors from '../src'
 
-it('should generate 1~100', () => {
+it('should generate 2~98', () => {
     let count = 0
     for (const colorName in colors) {
         const eachColorScale = colors[colorName as keyof typeof colors]
         const result = fillColorScale(eachColorScale)
         count += Object.keys(result).length
     }
-    expect(count).toBe(2002)
+    expect(count).toBe(1002)
 })
